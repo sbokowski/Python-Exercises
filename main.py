b3edbafc6ec769507b1e1cb50ddf6749c2cbc83e
shaun bokowski <79222959+sbokowski@users.noreply.github.com>
2025-05-20 18:34:14 -0400

    Create main.py
    Python tutorial , data types , special data types.  5/ 20/25

---
+++ main.py
@@ -0,0 +1,116 @@
+# Data Types - pythong has  several available
+#   first we have
+#   int
+# and int - represents an integer data type. its a
+#  whole number , positive, negative , or zero with out
+# any decimal component no decimals
+#  example  x = 10  int  or  y = -5 or z = 0
+#  floa†
+# we also have floats , in python float is a data
+# type that represents a float point number , its
+# used to represent  real numbers with decimal points
+# such as 3.14 , 2.718 or --0.5
+# bool
+# Which stands for boolean ,  a boolean is a data
+# that can have one of two value values.  True
+# or False .  often used in conditional statements
+# and loops to control the flow of program.  they
+# are named after George Boole, who pioneered
+# mathematical logic.
+# str
+# which stands for string , python strings are used
+# for representing textual data. A string is a
+#  sequence of characters enclosed in either single
+#  quotes '' or double quotes ""
+# list
+# python list is a versatile , mutable, ordered,
+#  sequence of items. IT is defined by enclosing
+# comma separated items within square brackets [ }
+# Lists cna contain elements of differ data types,
+# including numbers, string, booleans, and other
+# nested lists
+# tuple
+# what is a tuple in python universe. In python a
+# Data Types - pythong has  several available
+#   first we have
+#   int
+# and int - represents an integer data type. its a
+#  whole number , positive, negative , or zero with out
+# any decimal component no decimals
+#  example  x = 10  int  or  y = -5 or z = 0
+#  floa†
+# we also have floats , in python float is a data
+# type that represents a float point number , its
+# used to represent  real numbers with decimal points
+# such as 3.14 , 2.718 or --0.5
+# bool
+# Which stands for boolean ,  a boolean is a data
+# that can have one of two value values.  True
+# or False .  often used in conditional statements
+# and loops to control the flow of program.  they
+# are named after George Boole, who pioneered
+# mathematical logic.
+# str
+# which stands for string , python strings are used
+# for representing textual data. A string is a
+#  sequence of characters enclosed in either single
+#  quotes '' or double quotes ""
+# list
+# python list is a versatile , mutable, ordered,
+#  sequence of items. IT is defined by enclosing
+# comma separated items within square brackets [ }
+# Lists cna contian elements of differ data types,
+# including numbers, string, booleans, and other
+# nested lists
+# tuple
+# what is a tuple in python universe. In python a
+#
+# print(type(2 * 3))
+# print(type(6))
+# print(type(2 - 5))
+# print(type(2 / 4))
+#
+# print(2 ** 3)
+# print(5 // 4)
+# print(6 % 4) # in python this a modulo
+#
+# math functions
+# give the out of 2
+# print(round(3.9))
+# print(abs(-21))
+# operator precedence PEMDAS
+# what does that mean  Dont worry
+# the multiplied get done  first over adding
+# print(20 - 3 * 4)
+# first with be calculated is brackets ()
+# second will be the power of  % **
+# print((20 - 3) + 2 ** 2)
+# print((10 - 5) + 3 ** 3)
+# print(3 ** 3)
+# 3 x 3 is 9 x 3  = 27
+# In Python, __name__ is a special variable assigned to the name of
+# # # # the Python module by the interpreter. If your module is invoked
+# as a script, then the string '__main__' will automatically
+# be assigned to the special variable __name__ .
+# name = input('What is your game? ')
+# print(name)
+# name = input('What is a data type and list some types? ')
+# print("Data types store info and have values such as  " + name)
+# name = input('List a data type and its function ? ')
+# print('Data type store infor and have valuses such as ' + name)
+# fundamental data types
+# int and float
+print(type(6))
+print(type(2-4))
+print(type(2-5))
+print(type(2 * 4))
+print(type(2 / 4))
+#
+print(2 ** 3)
+print('This is a string ,enclosed in quotes')
+print(" String in quotes . ")
+print(5 // 4)
+print(6 % 4)
+
+
+
