4331345a9a17f72fd7a51d4f0d5ff7e6f1f19a2b
shaun bokowski <79222959+sbokowski@users.noreply.github.com>
2025-06-28 23:11:36 -0400

    Create main-2.py

---
+++ main-2.py
@@ -0,0 +1,46 @@
+# print("Welcome to main 2 file!")
+# sword_damage = 25
+# start_health = 30
+# end_health = start_health + sword_damage
+# # dont touch below
+# print(f"sams health is: {start_health}")
+# print(f'sam takes {sword_damage} damage...')
+# print(f"Sam's health is: {end_health}")
+# sword_damage = 25
+# start_health = 30
+#
+# augmented assign ment operator
+# some_value = 5
+# some_value -= 2
+# print(some_value)
+#   string "hi im a string"
+# Strings -work with Strings
+# string concatentation  add 2 strings together
+#
+# print("Here " + "We " + "Go")
+# print("Welcome " + "visitors! ")
+# unpacking the into 3 variables ,
+# coordinates = (1, 2, 3)
+# x, y, z = coordinates
+# print(z)
+#
+# coordinates = (1, 2, 3)
+#  x = coordinates[0]
+#  y = coordinates[1]
+#  z = coordinates[2]
+#  above example
+#   instead write it out like this below
+# , y, z = coordinates
+# print(y)
+#
+#  Type conversion
+# birth_year = input('birth_year: ')
+# print(type(birth_year))
+# # connvert in int
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+# show weight in kilograms
+weight_lbs = input('Weight (lbs):')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
