8d2dc9fa244c2fe420a571de5c15042fdd1426f5
shaun bokowski <79222959+sbokowski@users.noreply.github.com>
2025-06-29 23:54:26 -0400

    Update main-2.py

--- main-2.py
+++ main-2.py
@@ -44,3 +44,52 @@
 weight_lbs = input('Weight (lbs):')
 weight_kg = int(weight_lbs) * 0.45
 print(weight_kg)
+
+# june 29 2025
+# ready  for 100 days  
+# june 29 2025
+# where do we want to be in 30 days
+# Learn the basics
+#
+# Escape Sequence   Examples
+# new_climate = 'It\'s \"sunny in Detroit\" on\nweekends  \n by Gog  Ruffy'
+# print(new_climate)
+# # you need to get used to this syntax
+#
+# Formatted Strings Formatted Strings Formatted Strings
+#  examples
+# F strings
+# first_name = "Corey"
+# last_name = "Shafe"
+# place holders  what is going to get filed in,
+# sentence = "My name is {} {}".format(first_name, last_name)
+# print(sentence)
+# we have a string that has 2 place holders
+# this below is a simple f fstring
+# we can add our variables in our place holder in braces
+# so if we save this and run it
+# Using f strings with upper() - makes uppercase text
+# sentence= f'His name was {first_name.upper()} {last_name.upper()}'
+# print(sentence)
+tutor = "Python Quick Tip: F-Strings - How to Use Them and Advanced String Formatting"
+print(tutor)
+person = {'name': "Jenn", "age": 23}
+sentence = "My name is {} and I am  {} years old".format(person['name'], person ['age'])
+print(sentence)
+#
+# Exercises ; Printing Practice
+print('Hello world! Day 1 ')
+print('The location of this file, where you wrote the code')
+mistake = 'The red on your code , hover  over it will show problem'
+print(mistake)
+recipe_one = "1 Mix 500g of flour, 10g Yeast and 200ml Water in a bowl"
+recipe_two = "2 knead dough for 10 min."
+recipe_three = '3 Add 3g of salt.'
+recipe_four = '4 Leave to rise'
+print(recipe_one)
+print(recipe_two)
+print(recipe_three)
+print(recipe_four)
+print('\tHey everyone\nhello people')
+print('Hello' + ' ' 'Angels flying')
+
