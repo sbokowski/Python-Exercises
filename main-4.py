f5fef8dfb12ace2d14d34cf22ea4437310505296
shaun bokowski <79222959+sbokowski@users.noreply.github.com>
2025-07-04 08:48:04 -0400

    Create main-4.py

---
+++ main-4.py
@@ -0,0 +1,133 @@
+
+# july 1 2025
+# print some strings
+# print("Noe from Day 1")
+# print("This print statement is used for out strings")
+# print("Strings are strings of characters")
+# print("String concentenation is done with the + sign")
+# print("New lines can be created with a \n i am a new line! ")
+#
+# print() function
+# print(" The print function")
+# print("The python  print function")
+# print("The python awswome print function ")
+#
+# variable naming
+# now, the final thing to remember is that if you decide
+# to call your variable a name ,
+some_name = "Jackson"
+length = len(some_name)
+print(length)
+#
+student_name = "alexander"
+length = len(student_name)
+print(length)
+
+# later on, when you want to use this piece of data, you
+# use this name to refer to it.
+#
+#   input function
+#
+input("Are you ready to go? ")
+print("Lets go now")
+
+# the input function input()  input("a prompt for the user")
+# The input function looks pretty much identical to the
+#  print function , instead of the word print
+# and in some places you want even see a cursor until  you
+# click into the output area for user
+#
+# so , what can we do with this data inside our code
+# Now that way this works is when this line run by
+# the computer , it takes your command as understanding
+# that you want the user
+#
+#  input("Whats the name of the  game?")
+# so somebody to be able tok you want the user to be able
+# to type in an input and this area and whatever
+#
+# june 2 2025
+# input() function
+# and then onece it in their, the print function is going
+# to carry out the role and is going to add
+# the hello , the user types , the wil replace  the
+# hightligh  place - it wil end up with hello plus
+# concatenate . so we click this area, your cursor starts
+#  flashing , then we con type some test into the place
+# add an exclamation to
+# input() functin how to
+# print("Greetings " + input("what is your name. ") + "!")
+#
+# To find the length of a character in Python , we need
+# to clarify the term ,
+# the length of a string , which contain mulit characters
+# you use the built in len() , len(name-of_string)
+# len() find how many characters
+# name = ("Jack in the bed went quietly")
+# print(len(name))
+# #
+text = "dorito chips"
+print(len(text))
+#
+# switch variables  with 3 lines of code
+glass1 = "milk"
+milk = "glass1"
+print(milk)
+#
+#  Exercise Band name generator
+# Make a title for Band name generator
+ # Ask what name of city you grew up in?
+ # add a \n to the end of sentence so text show
+ # on the next line so your program looks more orgainized
+#
+# print("Welcome to The Band Name Generator")
+# city = input("Which city did you grow up in? \n ")
+# pet = input("What is the name of a pet? \n ")
+# print("Your band name could be: " + city + " " + pet)
+
+# input value will be lost if you dont append a value to it
+# now we have access to the city , we can use it later
+#  so we call what city to city = to assign what the user
+# typed , then i can print it use it later on
+# then we want to assign your pen name to a variable so we
+# can use it later to put the variables together
+# next to combine the name  of the city and the pet and
+# display and show them their band name.
+# by using the string concatenation - put 2 strings into one
+#
+# Next lesson onward
+# using the print(len('Character count"))
+#
+# print(len("Hello my darling"))
+# print(len("Yes , we can do that"))
+# use numbers ,what happens not quotes
+# error saying type error
+# TypeError: object of type 'int' has no len()
+# print(len(12234))
+# question  what will happen if we use numbers,
+# instead  of letters
+#
+# Data types  String , Integer , floats ,
+# boolean - type of data types
+#
+# Remember we start counting from 0 in binary
+# sub scripting -
+# So this metod of pulling out certain element
+# from a string is called sub scripting and the
+# nunber in between the square brackets determines
+# the character you are picking out.
+# 9
+print("Good day"[2])
+print('Any day'[4])
+print("Tommorow"[6])
+print("Good"[0])
+# # square brackets []  curly braces { }
+#
+# message = "One of python stength is diverse community "
+# print(message)
+#
+quality = "  ONe of python qualities"
+print(quality.strip())
+practice = "One of python quality is"
+print(practice.strip())
+
