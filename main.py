f950b2d834d5e51fbd6ac4a08880bdaa6793d078
shaun bokowski <79222959+sbokowski@users.noreply.github.com>
2025-10-04 22:33:38 -0400

    Update main.py
    python exercises

--- main.py
+++ main.py
@@ -120,5 +120,206 @@ print(5 + (4 * 10) / 2)
 print(5 + 4 * 10 // 2)
 print(33 / 4)
 print(32 / 4)
+#
+# edit on oct 4 / 25
+# new data type , complex which means
+# its name complex instead of int and
+# float we have this type for number
+#  bin()
+# print binary number 0b101 - which is 5
+# print(bin(5))
+# # this will return value of 5
+# print(int('0b101', 2))
+# print(int('0b101', 2))
+#
+# Variables
+# all programming languages have variable
+# lets say we have a program that store
+# iq is a variable
+# your IQ is 200
+# iq = 190
+# now we print id , it will be used , printed out
+# print(id)
+# remember variables are case sensitive
+# in python dont overwrite , dont use it again
+# dont overwrite , dont use a keyword for a variable
+# user_id = 100
+# print(user_id)
+# iq = 170
+# user_age = iq/4
+# a = user_age
+# print(user_age)
+#
+# Constants
+# a constants  does not change
+# PI = 3.14
+# PI = 0
+# print(PI)
+# a,b,c = 1,2,3
+# print(a)
+# print(b)
+# print(c)
+#
+# Expression vs Statement
+# if we create a piece of code here
+#  iq / 5 is the expression part of the
+# it's a piece of code , which produces
+# a value  100 / 5 equals 20
+# a statement on the other hand , is an
+# entire line  of code  userr_age = iq / 5
+# an entire line of code that performs som
+# sort of action
+#  code in action
+# iq = 190
+# user_age = iq / 5
+# print(user_age)
+#
+# Augmented Assignment operator
+# allows us to do some quite neat
+# example , it does what it is doing, understand?
+# add a value to another
+# some_value = 10
+# some_value = some_value + 20
+# print(some_value)
+# instead of a# add a value to another
+# # some_value = 10
+# # some_value = some_value + 20
+# # print(some_value)
+# # instead of above , below short hand  augmented  +=
+# #
+# some_value = 10
+# some_value += 2
+# print(some_value) above , below short hand  augmented  +=
+# works as * , + - . so on
+#
+#  code in action
+# some_value = 5
+# some_value += 2
+# print(some_value)
+#
+#  strings  - str
+# a sting is a piece of text for example , a string
+#  can b written with quotation or double quotes
+# Its a piece of text
+#  print(bin()) prints binary number
+# Complex number
+# use 2 for base for binary number
+# print(int('0b101', 2))
+# using variable exampole
+# const example with variable
+#
+# ab = 3.14
+# ab = 2*3
+# print(ab)
+#
+# f string conversion
+# temp_name = 67
+# print(f"The temperature is {(temp_name - 32) / 1.8:.2f} degrees in Celsius?")
+# temp_name = 67
+#
+#  Python functions examples
+#
+# print(f"The temperature is {(temp_name - 32) /1.8:.4f} degrees in Celsius")
+#
+# july 4 2025
+# Lets define a simple function that calculates
+# and display the results
+# a rectangles  area , we will use the keyword def
+#  use def to define a new keyword name for our function
+# descriptive keyword , since the goal and purpose of our
+# function is to calculate  Lets name it
+#  name it  cal_rect_area - functions  name
+#
+#
+# next we follow the functions name with parenthesis ()
+# function that
+# Calculates a rectangle area
+# Displays results
+# def calc_rect_area()
+# input function needs to perform it task
+# area requires in length and width , we can write
+#
+# inside is the () is the function input
+# finally we end the line a colon
+# next after is feature  with the next line is indented
+# Lets know fill in the line of the body of the function
+#
+# strip left space
+# quality = " 1. lstrip() remove left space"
+# print(quality.lstrip())
+# # right space strip
+# quality = "  2. rstrip() removes right space  "
+# print(quality.rstrip())
+# #
+# # strip al space
+# practice = " 3. strip() removes both space"
+# print(practice.strip())
+# # review what you do
+#
+#
+# practice = "  3 .lstrip ()  removes left space"
+# print(practice.lstrip())
+# practice = " 3. lstrip () removes left space"
+# print(practice.upper()
+# in python command print tells the
+# program display words or numbers
+# on the screen
+# coursera course
+#
+# the first thing we have in every programming language
+# reserved words , the first thing we have in every
+# programming  language is  reserved words
+# reserved words in python , use them that pythong
+# expects them to mean , expect them to mean in python
+# we cant make up a variable named in reserved words
+# reserved words  example
+# False await else import pass
+# reserved words are like words / communication to
+# talk to a dog - lets go for a walk - the dog
+# understand walk , similar  to reserved  words
+# x = 2  -  assignment  statement
+# x = x + 2  - assignment with expression
+# courser course / python for everybody
+# you can you get some work everyday done
+# m = 55
+# m = m - 1
+# print(m)
+#   comment a line  , cmd + /
+# sept 30 / 25 continue
+# conditional steps
+# x = 5
+# if x < 10:
+#     print('smaller')
+# if x > 20:
+#     print('bigger')
+# y = 10
+# if y < 12:
+#     print('smaller')
+# if y == 10:
+#     print('same number')
+# if y > 9:
+#     print("larger")
+#  some variables with float multiplication / simple
+# xyz = 35.0
+# abc = 12.50
+# cde = xyz * abc
+# print(cde)
+# aa = 34
+# bb = 3
+# cc = aa * bb
+# print(cc)
+# joe = 44
+# bob = 22
+# age_diff = joe - bob
+# print(age_diff)
+# variable naming - name accordingly
+# jj = 23
+# kk = jj % 5
+# print(kk)
+# jj = 28
+# kk = jj % 5
+# print(kk)
+x = (2 - 3 * 3) + ((4 + 3) + 5 - 4)
+print(x)
 
 
